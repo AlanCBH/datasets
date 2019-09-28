45f5ea35a6bfbe54d6b79c8e0fbb73c4dd864486
Cheng <binghui2@golubh1.campuscluster.illinois.edu>
2019-09-28 15:47:42 -0500

    code

---
+++ pyScript_collapse.py
@@ -0,0 +1,36 @@
+import os
+import string
+import numpy as np
+import argparse
+import dendropy
+def main(args):
+	estimatedPath = args.estimated
+    
+	outputfile = args.outputfile
+    
+	tax = dendropy.TaxonNamespace()
+	generated = dendropy.Tree.get(path=estimatedPath,
+                                      schema="newick",
+                                       rooting="force-unrooted",
+                                     taxon_namespace = tax)
+
+
+    
+	all_bips = generated.encode_bipartitions()
+	E_Map = generated.bipartition_edge_map
+	#support_values = {}
+	for bip in all_bips :
+        #if bip not in FalsePositives:
+		e = E_Map[bip]
+		if (e.head_node).label is not None and float((e.head_node).label) < 0.85:
+			e.collapse()
+    
+	generated.write(path=outputfile, schema="newick")
+        
+        
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="tree and its output path")
+	parser.add_argument("-t1", "--estimated", type=str,  required=True,
+                        help="estimated")
+	parser.add_argument("-o","--outputfile",type=str,required=True,help="outputfile")
+	main(parser.parse_args())
