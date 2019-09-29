1dc5d85ca6a7daff422de195e39317e8bb0e9c1e
Cheng <binghui2@golubh1.campuscluster.illinois.edu>
2019-09-29 13:13:05 -0500

    step2

--- pyScript_collapse.py
+++ pyScript_collapse.py
@@ -5,7 +5,7 @@ import argparse
 import dendropy
 def main(args):
 	estimatedPath = args.estimated
-    
+	threshold = args.num
 	outputfile = args.outputfile
     
 	tax = dendropy.TaxonNamespace()
@@ -22,8 +22,9 @@ def main(args):
 	for bip in all_bips :
         #if bip not in FalsePositives:
 		e = E_Map[bip]
-		if (e.head_node).label is not None and float((e.head_node).label) < 0.85:
+		if (e.head_node).label is not None and float((e.head_node).label) < threshold:
 			e.collapse()
+			#print((e.head_node).label)
     
 	generated.write(path=outputfile, schema="newick")
         
@@ -32,5 +33,6 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="tree and its output path")
 	parser.add_argument("-t1", "--estimated", type=str,  required=True,
                         help="estimated")
+	parser.add_argument("-n","--num",type=float, required=True, help="threshold")
 	parser.add_argument("-o","--outputfile",type=str,required=True,help="outputfile")
 	main(parser.parse_args())
