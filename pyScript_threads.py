e9cec111ca1041702e7bb7ff0bf4ed0016388406
Cheng <binghui2@golubh4.campuscluster.illinois.edu>
2019-10-17 12:37:55 -0500

    threads

---
+++ pyScript_threads.py
@@ -0,0 +1,23 @@
+import dendropy
+import numpy as np
+import os
+import sys
+import string
+import argparse
+
+def main(args):
+	alignmentPath = args.alignmentPath
+	dna = dendropy.DnaCharacterMatrix.get(
+            path=alignmentPath,
+            schema="fasta")
+	print(dna.max_sequence_size)
+	max_size = int(dna.max_sequence_size/500)
+	if max_size > 12:
+		max_size = 12
+	print(max_size)
+	return max_size
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="input path")
+	parser.add_argument("-a","--alignmentPath",type=str,required=True)
+	main(parser.parse_args())
